a84ceae2250509fde88aa56bec419e6eda9e02fe
Gourdol <maxime.gourdol@winzana.com>
2023-10-16 17:32:07 +0200

    feat(contact): implement contact domain

---
+++ src/Contact/Dto/ContactInputDto.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Sherl\Sdk\Contact\Dto;
+
+use JMS\Serializer\Annotation as Serializer;
+
+class ContactInputDto
+{
+  /**
+   * @var string
+   * @Serializer\Type("string")
+   */
+  public $name;
+
+  /**
+   * @var string
+   * @Serializer\Type("string")
+   */
+  public $email;
+
+  /**
+   * @var string
+   * @Serializer\Type("string")
+   */
+  public $message;
+}
