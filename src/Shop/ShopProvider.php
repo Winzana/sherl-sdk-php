602bbd368594e3cf2baa61fa1687eca9e90c818e
johan Affre <johan@winzana.com>
2023-11-15 10:38:37 +0100

    feat(SHER-681): add domain shop doc types and doc

--- src/Shop/ShopProvider.php
+++ src/Shop/ShopProvider.php
@@ -203,4 +203,104 @@ class ShopProvider
             'json'
         );
     }
+// order
+    public function cancelOrder(string $orderId, CancelOrderInputDto $cancelOrderDates): ?OrderResponse
+{
+    $response = $this->client->post(
+        "/api/shop/orders/{$orderId}/cancel",
+        [
+            "headers" => [
+                "Content-Type" => "application/json",
+            ],
+            RequestOptions::JSON => $cancelOrderDates
+        ]
+    );
+    if ($response->getStatusCode() >= 300) {
+        return $this->throwSherlShopException($response);
+    }
+
+    return SerializerFactory::getInstance()->deserialize(
+        $response->getBody()->getContents(),
+        OrderResponse::class,
+        'json'
+    );
+}
+
+public function getOrder(string $orderId): ?OrderResponse
+{
+    $response = $this->client->get("/api/shop/orders/{$orderId}");
+
+    if ($response->getStatusCode() != 200) {
+        return $this->throwSherlShopException($response);
+    }
+
+    return SerializerFactory::getInstance()->deserialize(
+        $response->getBody()->getContents(),
+        OrderResponse::class,
+        'json'
+    );
+}
+
+public function getOrders(OrderFindByDto $filters): ?Pagination
+{
+
+    $response = $this->client->get("/api/shop/orders", [
+        'query' => $filters
+    ]);
+
+    if ($response->getStatusCode() != 200) {
+        return $this->throwSherlShopException($response);
+    }
+
+    return SerializerFactory::getInstance()->deserialize(
+        $response->getBody()->getContents(),
+        Pagination::class,
+        'json'
+    );
+}
+public function getOrganizationOrders(string $organizationId, OrderFindByDto $filters): ?Pagination
+{
+
+    $response = $this->client->get(
+        "/api/shop/orders/list-to/{$organizationId}",
+        [
+            "headers" => [
+                "Content-Type" => "application/json",
+            ],
+            'query' => $filters
+        ]
+    );
+
+    if ($response->getStatusCode() != 200) {
+        return $this->throwSherlShopException($response);
+    }
+
+    return SerializerFactory::getInstance()->deserialize(
+        $response->getBody()->getContents(),
+        Pagination::class,
+        'json'
+    );
+}
+public function updateOrderStatus(string $orderId, OrderStatusEnum $status): ?OrderResponse
+{
+    $response = $this->client->post(
+        "/api/shop/orders/{$orderId}/status/:status",
+        [
+            "headers" => [
+                "Content-Type" => "application/json",
+            ],
+            RequestOptions::JSON => ['status' => $status]
+        ]
+    );
+
+    if ($response->getStatusCode() >= 300) {
+        return $this->throwSherlShopException($response);
+    }
+
+    return SerializerFactory::getInstance()->deserialize(
+        $response->getBody()->getContents(),
+        OrderResponse::class,
+        'json'
+    );
+}
 }
