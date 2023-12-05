381c20369e150540c5484f322f89f8a74268cef6
johan Affre <johan@winzana.com>
2023-12-05 15:37:38 +0100

    feat(SHER-693): add doc provider

--- src/Iam/IamProvider.php
+++ src/Iam/IamProvider.php
@@ -14,17 +14,32 @@ class IamProvider
     private Client $client;
     private array $endpoints;
 
+    /**
+     * IamProvider constructor.
+     * @param Client $client The HTTP client used to make requests.
+     * @param array $endpoints The endpoints used for IAM operations.
+     */
     public function __construct(Client $client, array $endpoints)
     {
         $this->client = $client;
         $this->endpoints = $endpoints;
     }
 
+    /**
+     * Throws an IAM exception based on the response.
+     * @param ResponseInterface $response The response from the HTTP client.
+     * @throws SherlException Throws a SherlException with response details.
+     */
     private function throwSherlIamException(ResponseInterface $response)
     {
         throw new SherlException(SherlException::DOMAIN, $response->getBody()->getContents(), $response->getStatusCode());
     }
-
+    /**
+     * Retrieves all IAM profiles matching the specified filters.
+     * @param array $filters The filters to apply to the IAM profiles retrieval.
+     * @return IamProfilesFilterDto The filtered IAM profiles.
+     * @throws SherlException If the response status code is not 200.
+     */
     public function getAllIamProfiles(array $filters): IamProfilesFilterDto
     {
         try {
@@ -44,7 +59,12 @@ class IamProvider
             throw new SherlException(SherlException::FETCH_FAILED, $e->getMessage());
         }
     }
-
+    /**
+     * Retrieves an IAM profile by its identifier.
+     * @param string $id The identifier of the IAM profile.
+     * @return ProfileDto The IAM profile data transfer object.
+     * @throws SherlException If the response status code is not 200 or fetching fails.
+     */
     public function getIamProfileById(string $id): ProfileDto
     {
         try {
@@ -70,7 +90,12 @@ class IamProvider
         }
     }
 
-
+    /**
+     * Retrieves an IAM role by its identifier.
+     * @param string $id The identifier of the IAM role.
+     * @return ProfileDto The IAM role data transfer object.
+     * @throws SherlException If the response status code is not 200 or fetching fails.
+     */
     public function getIamRoleById(string $id): ProfileDto
     {
         try {
