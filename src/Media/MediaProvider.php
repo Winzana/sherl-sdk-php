88602d649d7805c41e554e7862edf38b96d42171
johan Affre <johan@winzana.com>
2023-12-06 10:25:23 +0100

    feat(SHER-698): add doc provider

--- src/Media/MediaProvider.php
+++ src/Media/MediaProvider.php
@@ -13,17 +13,30 @@ use Sherl\Sdk\Common\SerializerFactory;
 class MediaProvider
 {
     private Client $client;
-
+    /**
+     * MediaProvider constructor.
+     * @param Client $client The HTTP client used to make requests.
+     */
     public function __construct(Client $client)
     {
         $this->client = $client;
     }
-
+    /**
+     * Throws a SherlMediaException with a custom message.
+     * @param ResponseInterface $response The response object from the HTTP client.
+     * @param string $message The custom error message.
+     * @throws SherlException
+     */
     private function throwSherlMediaException(ResponseInterface $response, string $message)
     {
         throw new SherlException(MediaErr::DOMAIN, $message, $response->getStatusCode());
     }
-
+    /**
+     * Deletes a file by its identifier.
+     * @param string $id The unique identifier of the file to delete.
+     * @return string The body content of the HTTP response.
+     * @throws SherlException If the file deletion fails.
+     */
     public function deleteFile(string $id): string
     {
         try {
@@ -40,7 +53,13 @@ class MediaProvider
             throw new SherlException(MediaErr::DELETE_FILE_FAILED, $e->getMessage());
         }
     }
-
+    /**
+     * Retrieves the details of a file by its identifier.
+     * @param string $id The unique identifier of the file to retrieve.
+     * @param array $query Additional query parameters for the request.
+     * @return array An associative array representing the file details.
+     * @throws SherlException If the file retrieval fails.
+     */
     public function getFile(string $id, array $query): array
     {
         try {
@@ -63,7 +82,12 @@ class MediaProvider
             throw new SherlException(MediaErr::GET_FILE_FAILED, $e->getMessage());
         }
     }
-
+    /**
+     * Uploads a file to the server.
+     * @param array $data The multipart file data to be uploaded.
+     * @return array An associative array representing the uploaded file details.
+     * @throws SherlException If the file upload fails.
+     */
     public function uploadFile(array $data): array
     {
         try {
