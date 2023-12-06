ed0f70175ef30f567bae68606ca4f56120ec7a69
johan Affre <johan@winzana.com>
2023-12-06 09:50:38 +0100

    feat(SHER-698): add doc provider

--- src/Place/PlaceProvider.php
+++ src/Place/PlaceProvider.php
@@ -10,11 +10,22 @@ class PlaceProvider
 {
     private Client $client;
 
+    /**
+     * PlaceProvider constructor.
+     * @param Client $client The HTTP client used to send requests to the API.
+     */
     public function __construct(Client $client)
     {
         $this->client = $client;
     }
-
+    /**
+     * Retrieves a list of places with pagination and optional filters.
+     * @param int $page The page number for pagination.
+     * @param int $itemsPerPage The number of items to display per page.
+     * @param array $filters Optional filters to apply to the place retrieval.
+     * @return array An array of places matching the criteria.
+     * @throws SherlException If the fetching of places fails.
+     */
     public function getPlaces(int $page = 1, int $itemsPerPage = 10, array $filters = []): array
     {
         try {
@@ -40,7 +51,11 @@ class PlaceProvider
             throw new SherlException(PlaceErr::FETCH_FAILED, $e->getMessage());
         }
     }
-
+    /**
+     * Handles the response from the API call.
+     * @param ResponseInterface $response The response from the API call.
+     * @throws SherlException If the response status code is not 200.
+     */
     private function handleResponse(ResponseInterface $response)
     {
         if ($response->getStatusCode() !== 200) {
