e47fb4344fdcf8d7ad8ed2277216bf5a30f0712b
johan Affre <johan@winzana.com>
2024-02-06 15:41:27 +0100

    feat(SHER-811): fix error domain bug-report

--- src/BugReport/BugReportProvider.php
+++ src/BugReport/BugReportProvider.php
@@ -14,96 +14,132 @@ use Sherl\Sdk\BugReport\Dto\CreateBugReportInputDto;
 use Sherl\Sdk\Common\SerializerFactory;
 use Sherl\Sdk\Common\Error\SherlException;
 
+use Exception;
+
+use Sherl\Sdk\Common\Error\ErrorFactory;
+use Sherl\Sdk\Common\Error\ErrorHelper;
+use Sherl\Sdk\BugReport\Errors\BugReportErr;
+
 class BugReportProvider
 {
     public const DOMAIN = "Bug report";
 
     private Client $client;
+    private ErrorFactory $errorFactory;
 
     public function __construct(Client $client)
     {
         $this->client = $client;
-    }
-
-    /**
-     * @throws SherlException
-     */
-    private function throwSherlBugReportContactException(ResponseInterface $response): SherlException
-    {
-        throw new SherlException(BugReportProvider::DOMAIN, $response->getBody()->getContents());
+        $this->errorFactory = new ErrorFactory(self::DOMAIN, BugReportErr::$errors);
     }
 
     public function createBugReport(CreateBugReportInputDto $createBugReportInput): ?BugReportOutputDto
     {
-        $response = $this->client->post('/api/bug-reports', [
-          "headers" => [
-            "Content-Type" => "application/json",
-          ],
-          RequestOptions::JSON => [
-            "osName" => $createBugReportInput->osName,
-            "browserName" => $createBugReportInput->browserName,
-            "contactEmail" => $createBugReportInput->contactEmail,
-            "message" => $createBugReportInput->message,
-            "windowHeight" => $createBugReportInput->windowHeight,
-            "windowWidth" => $createBugReportInput->windowWidth,
-          ]
-        ]);
+        try {
+            $response = $this->client->post('/api/bug-reports', [
+              "headers" => [
+                "Content-Type" => "application/json",
+              ],
+              RequestOptions::JSON => [
+                "osName" => $createBugReportInput->osName,
+                "browserName" => $createBugReportInput->browserName,
+                "contactEmail" => $createBugReportInput->contactEmail,
+                "message" => $createBugReportInput->message,
+                "windowHeight" => $createBugReportInput->windowHeight,
+                "windowWidth" => $createBugReportInput->windowWidth,
+              ]
+            ]);
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlBugReportContactException($response);
+            return SerializerFactory::getInstance()->deserialize(
+                $response->getBody()->getContents(),
+                BugReportOutputDto::class,
+                'json'
+            );
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(BugReportErr::CREATE_BUG_REPORT_FORBIDDEN);
+                }
+            }
+            throw $this->errorFactory->create(BugReportErr::CREATE_BUG_REPORT_FAILED);
         }
 
-        return SerializerFactory::getInstance()->deserialize(
-            $response->getBody()->getContents(),
-            BugReportOutputDto::class,
-            'json'
-        );
     }
 
     public function getBugReportById(string $bugReportId): ?BugReportOutputDto
     {
-        $response = $this->client->get("/api/bug-reports/$bugReportId", [
-          "headers" => [
-            "Content-Type" => "application/json",
-          ],
-        ]);
+        try {
+            $response = $this->client->get("/api/bug-reports/$bugReportId", [
+              "headers" => [
+                "Content-Type" => "application/json",
+              ],
+            ]);
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlBugReportContactException($response);
+            return SerializerFactory::getInstance()->deserialize(
+                $response->getBody()->getContents(),
+                BugReportOutputDto::class,
+                'json'
+            );
+
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(BugReportErr::GET_BUG_REPORT_BY_ID_FORBIDDEN);
+                    case 404:
+                        throw $this->errorFactory->create(BugReportErr::BUG_REPORT_NOT_FOUND);
+                }
+            }
+            throw $this->errorFactory->create(BugReportErr::GET_BUG_REPORT_BY_ID_FAILED);
         }
 
-        return SerializerFactory::getInstance()->deserialize(
-            $response->getBody()->getContents(),
-            BugReportOutputDto::class,
-            'json'
-        );
     }
 
     public function getBugReports(BugReportListInputDto $bugReportListInput): ?BugReportListOutputDto
     {
-        $response = $this->client->get('/api/bug-reports', [
-          "headers" => [
-            "Content-Type" => "application/json",
-          ],
-          RequestOptions::QUERY => [
-            "page" => $bugReportListInput->page,
-            "itemsPerPage" => $bugReportListInput->itemsPerPage,
-            "osName" => $bugReportListInput->osName,
-            "browserName" => $bugReportListInput->browserName,
-            "contactEmail" => $bugReportListInput->contactEmail,
-            "windowHeight" => $bugReportListInput->windowHeight,
-            "windowWidth" => $bugReportListInput->windowWidth
-          ]
-        ]);
+        try {
+            $response = $this->client->get('/api/bug-reports', [
+              "headers" => [
+                "Content-Type" => "application/json",
+              ],
+              RequestOptions::QUERY => [
+                "page" => $bugReportListInput->page,
+                "itemsPerPage" => $bugReportListInput->itemsPerPage,
+                "osName" => $bugReportListInput->osName,
+                "browserName" => $bugReportListInput->browserName,
+                "contactEmail" => $bugReportListInput->contactEmail,
+                "windowHeight" => $bugReportListInput->windowHeight,
+                "windowWidth" => $bugReportListInput->windowWidth
+              ]
+            ]);
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlBugReportContactException($response);
+            return SerializerFactory::getInstance()->deserialize(
+                $response->getBody()->getContents(),
+                BugReportListOutputDto::class,
+                'json'
+            );
+
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(BugReportErr::GET_BUG_REPORTS_FORBIDDEN);
+                }
+            }
+            throw $this->errorFactory->create(BugReportErr::GET_BUG_REPORTS_FAILED);
         }
-
-        return SerializerFactory::getInstance()->deserialize(
-            $response->getBody()->getContents(),
-            BugReportListOutputDto::class,
-            'json'
-        );
     }
 }
