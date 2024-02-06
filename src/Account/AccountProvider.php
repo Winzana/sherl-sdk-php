cb2f641a0508ea248e24750559516249e40c7a39
johan Affre <johan@winzana.com>
2024-02-06 10:24:46 +0100

    feat(SHER-812): fix error domain account

--- src/Account/AccountProvider.php
+++ src/Account/AccountProvider.php
@@ -8,47 +8,64 @@ use Sherl\Sdk\Account\Dto\AccountOutputDto;
 use Sherl\Sdk\Account\Dto\CreateAccountInputDto;
 use Sherl\Sdk\Common\Error\SherlException;
 use Sherl\Sdk\Common\SerializerFactory;
+use Sherl\Sdk\Common\Error\ErrorFactory;
+use Sherl\Sdk\Common\Error\ErrorHelper;
+use Sherl\Sdk\Account\Errors\AccountErr;
+use Exception;
 
 class AccountProvider
 {
     public const DOMAIN = "Account";
     private Client $client;
 
+    private ErrorFactory $errorFactory;
     public function __construct(Client $client)
     {
         $this->client = $client;
+        $this->errorFactory = new ErrorFactory(self::DOMAIN, AccountErr::$errors);
     }
 
     public function createAccount(CreateAccountInputDto $createAccount): ?AccountOutputDto
     {
-        $response = $this->client->post('/api/accounts', [
-          'headers' => [
-            'Content-Type' => 'application/json'
-          ],
-          RequestOptions::JSON => [
-            'hosts' => $createAccount->hosts,
-            'ips' => $createAccount->ips,
-            'firstName' => $createAccount->firstName,
-            'lastName' => $createAccount->lastName,
-            'legalName' => $createAccount->legalName,
-            'email' => $createAccount->email,
-            'mobilePhoneNumber' => $createAccount->mobilePhoneNumber,
-            'birthDate' => $createAccount->birthDate,
-            'gender' => $createAccount->gender,
-            'location' => $createAccount->location,
-            'password' => $createAccount->password,
-            'passwordRepeat' => $createAccount->passwordRepeat,
-          ]
-        ]);
 
-        if ($response->getStatusCode() >= 300) {
-            throw new SherlException(AccountProvider::DOMAIN, $response->getBody()->getContents());
+        try {
+            $response = $this->client->post('/api/accounts', [
+              'headers' => [
+                'Content-Type' => 'application/json'
+              ],
+              RequestOptions::JSON => [
+                'hosts' => $createAccount->hosts,
+                'ips' => $createAccount->ips,
+                'firstName' => $createAccount->firstName,
+                'lastName' => $createAccount->lastName,
+                'legalName' => $createAccount->legalName,
+                'email' => $createAccount->email,
+                'mobilePhoneNumber' => $createAccount->mobilePhoneNumber,
+                'birthDate' => $createAccount->birthDate,
+                'gender' => $createAccount->gender,
+                'location' => $createAccount->location,
+                'password' => $createAccount->password,
+                'passwordRepeat' => $createAccount->passwordRepeat,
+              ]
+            ]);
+
+            return SerializerFactory::getInstance()->deserialize(
+                $response->getBody()->getContents(),
+                AccountOutputDto::class,
+                'json'
+            );
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(AccountErr::CREATE_ACCOUNT_FORBIDDEN);
+                }
+            }
+            throw $this->errorFactory->create(AccountErr::CREATE_ACCOUNT_FAILED);
         }
-
-        return SerializerFactory::getInstance()->deserialize(
-            $response->getBody()->getContents(),
-            AccountOutputDto::class,
-            'json'
-        );
     }
 }
