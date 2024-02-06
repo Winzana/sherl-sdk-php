eb52db2cabc193ef80d18de6c72a2a5c410b9de2
johan Affre <johan@winzana.com>
2024-02-06 16:57:45 +0100

    feat(SHER-806): fix error domain contact

--- src/Contact/ContactProvider.php
+++ src/Contact/ContactProvider.php
@@ -7,63 +7,83 @@ use GuzzleHttp\RequestOptions;
 use Psr\Http\Message\ResponseInterface;
 use Sherl\Sdk\Common\Error\SherlException;
 use Sherl\Sdk\Contact\Dto\ContactInputDto;
+use Exception;
+
+use Sherl\Sdk\Common\Error\ErrorFactory;
+use Sherl\Sdk\Common\Error\ErrorHelper;
+use Sherl\Sdk\Contact\Errors\ContactErr;
 
 class ContactProvider
 {
     public const DOMAIN = "Contact";
-
+    private ErrorFactory $errorFactory;
     private Client $client;
 
     public function __construct(Client $client)
     {
         $this->client = $client;
-    }
-
-    /**
-     * @throws SherlException
-     */
-    private function throwSherlContactException(ResponseInterface $response): SherlException
-    {
-        throw new SherlException(ContactProvider::DOMAIN, $response->getBody()->getContents());
+        $this->errorFactory = new ErrorFactory(self::DOMAIN, ContactErr::$errors);
     }
 
     public function sendContact(ContactInputDto $contactInput): string
     {
-        $response = $this->client->post('/api/contact', [
-          "headers" => [
-            "Content-Type" => "application/json",
-          ],
-          RequestOptions::JSON => [
-            "name" => $contactInput->name,
-            "email" => $contactInput->email,
-            "message" => $contactInput->message
-          ]
-        ]);
+        try {
+            $response = $this->client->post('/api/contact', [
+              "headers" => [
+                "Content-Type" => "application/json",
+              ],
+              RequestOptions::JSON => [
+                "name" => $contactInput->name,
+                "email" => $contactInput->email,
+                "message" => $contactInput->message
+              ]
+            ]);
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlContactException($response);
+            return $response->getBody()->getContents();
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(ContactErr::SEND_CONTACT_FORBIDDEN);
+                }
+            }
+            throw $this->errorFactory->create(ContactErr::SEND_CONTACT_FAILED);
         }
 
-        return $response->getBody()->getContents();
     }
 
     public function contactPerson(string $id, ContactInputDto $contactInput): string
     {
-        $response = $this->client->post("/api/contact/$id", [
-          "headers" => [
-            "Content-Type" => "application/json",
-          ],
-          RequestOptions::JSON => [
-            "name" => $contactInput->name,
-            "email" => $contactInput->email,
-            "message" => $contactInput->message
-          ]
-        ]);
+        try {
+            $response = $this->client->post("/api/contact/$id", [
+              "headers" => [
+                "Content-Type" => "application/json",
+              ],
+              RequestOptions::JSON => [
+                "name" => $contactInput->name,
+                "email" => $contactInput->email,
+                "message" => $contactInput->message
+              ]
+            ]);
+            return $response->getBody()->getContents();
+        } catch (\Exception $err) {
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlContactException($response);
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(ContactErr::CONTACT_PERSON_FORBIDDEN);
+                    case 404:
+                        throw $this->errorFactory->create(ContactErr::CONTACT_NOT_FOUND);
+                }
+            }
+            throw $this->errorFactory->create(ContactErr::CONTACT_PERSON_FAILED);
         }
-
-        return $response->getBody()->getContents();
     }
 }
