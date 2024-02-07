8bcd9d05ed73e86b301fa6e9e5f0940de77b32b5
johan Affre <johan@winzana.com>
2024-02-07 11:11:33 +0100

    feat(SHER-808): fix error domain claim

--- src/Claim/ClaimProvider.php
+++ src/Claim/ClaimProvider.php
@@ -17,201 +17,297 @@ use Sherl\Sdk\Claim\Dto\UpdateClaimInputDto;
 use Sherl\Sdk\Common\Error\SherlException;
 use Sherl\Sdk\Common\SerializerFactory;
 
+use Exception;
+
+use Sherl\Sdk\Common\Error\ErrorFactory;
+use Sherl\Sdk\Common\Error\ErrorHelper;
+use Sherl\Sdk\Claim\Errors\ClaimErr;
+
 class ClaimProvider
 {
     public const DOMAIN = "Claim";
-
+    private ErrorFactory $errorFactory;
     private Client $client;
 
     public function __construct(Client $client)
     {
         $this->client = $client;
-    }
-
-    /**
-     * @throws SherlException
-     */
-    private function throwSherlClaimException(ResponseInterface $response): SherlException
-    {
-        throw new SherlException(ClaimProvider::DOMAIN, $response->getBody()->getContents());
+        $this->errorFactory = new ErrorFactory(self::DOMAIN, ClaimErr::$errors);
     }
 
     public function createClaim(CreateClaimInput $createClaim): ?ClaimOutputDto
     {
-        $id = $createClaim->id;
-        $response = $this->client->post(
-            "/api/claims/$id",
-            [
-            "headers" => [
-              "Content-Type" => "application/json",
-            ],
-            RequestOptions::JSON => [
-              'id' => $createClaim->id,
-              'personId' => $createClaim->personId,
-              'issueTitle' => $createClaim->issueTitle,
-              'issueMessage' => $createClaim->issueMessage,
-              'schedules' => $createClaim->schedules,
-            ]
+        try {
+            $id = $createClaim->id;
+            $response = $this->client->post(
+                "/api/claims/$id",
+                [
+                "headers" => [
+                  "Content-Type" => "application/json",
+                ],
+                RequestOptions::JSON => [
+                  'id' => $createClaim->id,
+                  'personId' => $createClaim->personId,
+                  'issueTitle' => $createClaim->issueTitle,
+                  'issueMessage' => $createClaim->issueMessage,
+                  'schedules' => $createClaim->schedules,
+                ]
       ]
-        );
+            );
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlClaimException($response);
+            return SerializerFactory::getInstance()->deserialize(
+                $response->getBody()->getContents(),
+                ClaimOutputDto::class,
+                'json'
+            );
+
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(ClaimErr::CREATE_CLAIM_FORBIDDEN);
+                    case 404:
+                        throw $this->errorFactory->create(ClaimErr::CLAIM_NOT_FOUND);
+                }
+            }
+            throw $this->errorFactory->create(ClaimErr::CREATE_CLAIM_ERROR);
         }
 
-        return SerializerFactory::getInstance()->deserialize(
-            $response->getBody()->getContents(),
-            ClaimOutputDto::class,
-            'json'
-        );
     }
 
     public function updateClaim(string $claimId, UpdateClaimInputDto $updateClaim): ?ClaimOutputDto
     {
-        $response = $this->client->put(
-            "/api/claims/$claimId",
-            [
-            "headers" => [
-              "Content-Type" => "application/json",
-            ],
-            RequestOptions::JSON => [
-              'status' => $updateClaim->status,
-            ]
+        try {
+            $response = $this->client->put(
+                "/api/claims/$claimId",
+                [
+                "headers" => [
+                  "Content-Type" => "application/json",
+                ],
+                RequestOptions::JSON => [
+                  'status' => $updateClaim->status,
+                ]
       ]
-        );
+            );
+            return SerializerFactory::getInstance()->deserialize(
+                $response->getBody()->getContents(),
+                ClaimOutputDto::class,
+                'json'
+            );
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlClaimException($response);
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(ClaimErr::UPDATE_CLAIM_FORBIDDEN);
+                    case 404:
+                        throw $this->errorFactory->create(ClaimErr::CLAIM_NOT_FOUND);
+                }
+            }
+            throw $this->errorFactory->create(ClaimErr::UPDATE_CLAIM_ERROR);
         }
 
-        return SerializerFactory::getInstance()->deserialize(
-            $response->getBody()->getContents(),
-            ClaimOutputDto::class,
-            'json'
-        );
     }
 
     public function getClaim(string $claimId): ?ClaimOutputDto
     {
-        $response = $this->client->get(
-            "/api/claims/$claimId",
-            [
-            "headers" => [
-              "Content-Type" => "application/json",
-            ],
+        try {
+            $response = $this->client->get(
+                "/api/claims/$claimId",
+                [
+                "headers" => [
+                  "Content-Type" => "application/json",
+                ],
       ]
-        );
+            );
+            return SerializerFactory::getInstance()->deserialize(
+                $response->getBody()->getContents(),
+                ClaimOutputDto::class,
+                'json'
+            );
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlClaimException($response);
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(ClaimErr::GET_CLAIM_BY_ID_FORBIDDEN);
+                    case 404:
+                        throw $this->errorFactory->create(ClaimErr::CLAIM_NOT_FOUND);
+
+                }
+            }
+            throw $this->errorFactory->create(ClaimErr::GET_CLAIM_BY_ID_FAILED);
         }
-
-        return SerializerFactory::getInstance()->deserialize(
-            $response->getBody()->getContents(),
-            ClaimOutputDto::class,
-            'json'
-        );
     }
 
     public function getClaims(FindClaimsInputDto $filters): ?ClaimsResultOutputDto
     {
-        $response = $this->client->get(
-            "/api/claims",
-            [
-            "headers" => [
-              "Content-Type" => "application/json",
-            ],
-            RequestOptions::QUERY => [
-              'filters' => $filters,
-              'page' => $filters->page,
-              'itemsPerPage' => $filters->itemsPerPage,
-            ]
+        try {
+            $response = $this->client->get(
+                "/api/claims",
+                [
+                "headers" => [
+                  "Content-Type" => "application/json",
+                ],
+                RequestOptions::QUERY => [
+                  'filters' => $filters,
+                  'page' => $filters->page,
+                  'itemsPerPage' => $filters->itemsPerPage,
+                ]
       ]
-        );
+            );
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlClaimException($response);
+            return SerializerFactory::getInstance()->deserialize(
+                $response->getBody()->getContents(),
+                ClaimsResultOutputDto::class,
+                'json'
+            );
+
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(ClaimErr::GET_ALL_CLAIM_FORBIDDEN);
+
+                }
+            }
+            throw $this->errorFactory->create(ClaimErr::GET_ALL_CLAIM_FAILED);
         }
 
-        return SerializerFactory::getInstance()->deserialize(
-            $response->getBody()->getContents(),
-            ClaimsResultOutputDto::class,
-            'json'
-        );
     }
 
     public function findClaimBy(ClaimFindOneByInputDto $findClaimOneBy): ?ClaimOutputDto
     {
-        $response = $this->client->get(
-            "/api/claims/find-one-by",
-            [
-            "headers" => [
-              "Content-Type" => "application/json",
-            ],
-            RequestOptions::QUERY => [
-              'consumerId' => $findClaimOneBy->consumerId,
-              'orderId' => $findClaimOneBy->orderId,
-              'personId' => $findClaimOneBy->personId,
-              'id' => $findClaimOneBy->id,
-            ]
+        try {
+            $response = $this->client->get(
+                "/api/claims/find-one-by",
+                [
+                "headers" => [
+                  "Content-Type" => "application/json",
+                ],
+                RequestOptions::QUERY => [
+                  'consumerId' => $findClaimOneBy->consumerId,
+                  'orderId' => $findClaimOneBy->orderId,
+                  'personId' => $findClaimOneBy->personId,
+                  'id' => $findClaimOneBy->id,
+                ]
       ]
-        );
+            );
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlClaimException($response);
+            return SerializerFactory::getInstance()->deserialize(
+                $response->getBody()->getContents(),
+                ClaimOutputDto::class,
+                'json'
+            );
+
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(ClaimErr::FIND_CLAIM_BY_FORBIDDEN);
+
+                }
+            }
+            throw $this->errorFactory->create(ClaimErr::FIND_CLAIM_BY_FAILED);
         }
 
-        return SerializerFactory::getInstance()->deserialize(
-            $response->getBody()->getContents(),
-            ClaimOutputDto::class,
-            'json'
-        );
     }
 
     public function replyToClaim(string $claimId, ReplyToClaimInputDto $replyToClaim): ?ClaimOutputDto
     {
-        $response = $this->client->post(
-            "/api/claims/$claimId/reply",
-            [
-            "headers" => [
-              "Content-Type" => "application/json",
-            ],
-            RequestOptions::JSON => [
-              'content' => $replyToClaim->content,
-            ]
+        try {
+            $response = $this->client->post(
+                "/api/claims/$claimId/reply",
+                [
+                "headers" => [
+                  "Content-Type" => "application/json",
+                ],
+                RequestOptions::JSON => [
+                  'content' => $replyToClaim->content,
+                ]
       ]
-        );
+            );
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlClaimException($response);
+            return SerializerFactory::getInstance()->deserialize(
+                $response->getBody()->getContents(),
+                ClaimOutputDto::class,
+                'json'
+            );
+
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(ClaimErr::REPLY_CLAIM_FORBIDDEN);
+                    case 404:
+                        throw $this->errorFactory->create(ClaimErr::CLAIM_NOT_FOUND);
+
+                }
+            }
+            throw $this->errorFactory->create(ClaimErr::REPLY_CLAIM_FAILED);
         }
 
-        return SerializerFactory::getInstance()->deserialize(
-            $response->getBody()->getContents(),
-            ClaimOutputDto::class,
-            'json'
-        );
+
     }
 
     public function refundClaim(string $claimId): ?ClaimOutputDto
     {
-        $response = $this->client->post(
-            "/api/claims/$claimId/refund",
-            [
-            "headers" => [
-              "Content-Type" => "application/json",
-            ],
+        try {
+            $response = $this->client->post(
+                "/api/claims/$claimId/refund",
+                [
+                "headers" => [
+                  "Content-Type" => "application/json",
+                ],
       ]
-        );
+            );
 
-        if ($response->getStatusCode() >= 300) {
-            $this->throwSherlClaimException($response);
+            return SerializerFactory::getInstance()->deserialize(
+                $response->getBody()->getContents(),
+                ClaimOutputDto::class,
+                'json'
+            );
+        } catch (\Exception $err) {
+
+            if ($err instanceof \GuzzleHttp\Exception\ClientException) {
+
+                $response = $err->getResponse();
+                $statusCode = $response->getStatusCode();
+                switch ($statusCode) {
+                    case 403:
+                        throw $this->errorFactory->create(ClaimErr::REFUND_CLAIM_FORBIDDEN);
+                    case 404:
+                        throw $this->errorFactory->create(ClaimErr::CLAIM_NOT_FOUND);
+
+                }
+            }
+            throw $this->errorFactory->create(ClaimErr::REFUND_CLAIM_FAILED);
         }
 
-        return SerializerFactory::getInstance()->deserialize(
-            $response->getBody()->getContents(),
-            ClaimOutputDto::class,
-            'json'
-        );
+
     }
 }
